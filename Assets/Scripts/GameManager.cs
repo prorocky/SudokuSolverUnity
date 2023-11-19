4e8006573132b5a89946cdc812b72840f769f3b6
prorocky <prorocky.2000@gmail.com>
2023-11-19 15:24:46 -0800

    creating structs for board, region, and tile. Lunch Break

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+struct Board
+{
+    
+}
+
+struct Region
+{
+    Tile[,] tiles;
+}
+
+struct Tile
+{
+    Color currentColor;
+    TextMesh text;
+}
+
+
 public class GameManager : MonoBehaviour
 {
+    // magic number for length of sudoku row/col
+    const int N = 9;
+
+    // GameObject board that is used in game
+    
+
+    // board for calculations
+    int[,] intBoard;
+
+    Color selectedTileColor = new Color(0.7f, 0.7f, 1f);
+    Color defaultTileColor = Color.white;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // initialize board
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                intBoard[i,j] = 0;
+            }
+        }
     }
 
     // Update is called once per frame
