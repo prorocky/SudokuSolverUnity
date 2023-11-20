bf233cbdde96a2cecc3be2246c0a5ed646e2c074
prorocky <prorocky.2000@gmail.com>
2023-11-19 16:41:36 -0800

    properly initialize and update board

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,30 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-struct Board
-{
-    
-}
-
-struct Region
-{
-    Tile[,] tiles;
-}
-
-struct Tile
-{
-    Color currentColor;
-    TextMesh text;
-}
-
-
 public class GameManager : MonoBehaviour
 {
     // magic number for length of sudoku row/col
     const int N = 9;
+    // magic number for number of regions on board
+    const int R = 3;
 
     // GameObject board that is used in game
-    
+    [SerializeField]
+    GameObject BOARD;
 
     // board for calculations
     int[,] intBoard;
@@ -35,13 +21,49 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // initialize board
-        for (int i = 0; i < N; i++)
+        intBoard = new int[9,9];
+        // in order to assign to int array, need to keep track of rows and cols of regions AND tiles
+        for (int regRow = 0; regRow < R; regRow++)
         {
-            for (int j = 0; j < N; j++)
+            for (int regCol = 0; regCol < R; regCol++)
             {
-                intBoard[i,j] = 0;
+                // current region
+                // GameObject region = BOARD.transform.GetChild(regRow * R + regCol).gameObject;
+                for (int tileRow = 0; tileRow < R; tileRow++)
+                {
+                    for (int tileCol = 0; tileCol < R; tileCol++)
+                    {
+                        // current tile
+                        // GameObject tile = region.transform.GetChild(tileRow * R + tileCol).gameObject;
+                        intBoard[regRow * R + tileRow, regCol * R + tileCol] = 0;
+                    }
+                }
             }
+        }
+        updateBoard();
+    }
+
+    void updateBoard() {
+        for (int regRow = 0; regRow < R; regRow++)
+        {
+            for (int regCol = 0; regCol < R; regCol++)
+            {
+                // current region
+                GameObject region = BOARD.transform.GetChild(regRow * R + regCol).gameObject;
+                for (int tileRow = 0; tileRow < R; tileRow++)
+                {
+                    for (int tileCol = 0; tileCol < R; tileCol++)
+                    {
+                        // current tile
+                        GameObject tile = region.transform.GetChild(tileRow * R + tileCol).gameObject;
+                        // print(regRow * R + tileRow + ", " + (regCol * R + tileCol));
+                        int val = intBoard[regRow * R + tileRow, regCol * R + tileCol];
+                        // print(tile.GetComponentInChildren<TMPro.TextMeshProUGUI>());
+                        tile.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = (val != 0) ? val.ToString() : "";
+                    }
+                }
+            }
+            
         }
     }
 
