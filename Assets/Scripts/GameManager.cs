6a26e556b05d9e1c25460c0efc012a19790e85e8
prorocky <prorocky.2000@gmail.com>
2023-11-20 22:59:04 -0800

    finished main functionality. added debugging capability to vscode

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    class instruction
+    {
+        public int row;
+        public int col;
+        public int num;
+
+        public instruction(int row, int col, int num)
+        {
+            this.row = row;
+            this.col = col;
+            this.num = num;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1}): {2}", row, col, num);
+        }
+    }
+
+
     // magic number for length of sudoku row/col
     const int N = 9;
-    // magic number for number of regions on board
+    // magic number for sqrt(N)
     const int R = 3;
 
     // GameObject board that is used in game
@@ -14,28 +35,97 @@ public class GameManager : MonoBehaviour
     GameObject BOARD;
 
     // board for calculations
-    static int[,] intBoard;
+    int[,] intBoard;
+
+    // this list will hold the list of instructions to then display onto the board in game scene
+    List<instruction> list;
 
     Color selectedTileColor = new Color(0.7f, 0.7f, 1f);
     Color defaultTileColor = Color.white;
+    // delay for coroutines, will be mutable via scroller in game scene
+    [SerializeField]
+    float delay = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
         intBoard = new int[9,9];
+        list = new List<instruction>();
         clearBoard();
         generateBoard();
+
         updateBoard();
+
+        solve(intBoard, true);
+        StartCoroutine("showAlgorithm");
 
     }
 
-    void generateBoard() {
+    void generateSolvedBoard() {
         int r = Random.Range(0, N-1);
         int c = Random.Range(0, N-1);
         int n = Random.Range(1, N);
 
         intBoard[r, c] = n;
 
-        solve(intBoard);
+        solve(intBoard, false);
+    }
+
+    void generateBoard() {
+        generateSolvedBoard();
+
+        // delete 56 at random
+        int numToDelete = 56;
+        // instead of 1 random between 0 and N*N, 2 random for row and col to add more randomness
+        int rng1, rng2;
+        while (numToDelete > 0)
+        {
+            rng1 = Random.Range(0, N);
+            rng2 = Random.Range(0, N);
+
+            if (intBoard[rng1, rng2] != 0) {
+                intBoard[rng1, rng2] = 0;
+                numToDelete--;
+            }
+            // StartCoroutine("deleteNumbersSlowly");
+        }
+    }
+
+    // creating method AND coroutine doing the same thing except the coroutine has a delay
+    // the delay will be able to be selected as a choice from the "gameplay" scene to watch the numbers get deleted slowly
+    void deleteNumbers() {
+        // delete 56 at random
+        int numToDelete = 56;
+        // instead of 1 random between 0 and N*N, 2 random for row and col to add more randomness
+        int rng1, rng2;
+        while (numToDelete > 0)
+        {
+            rng1 = Random.Range(0, N);
+            rng2 = Random.Range(0, N);
+
+            if (intBoard[rng1, rng2] != 0) {
+                intBoard[rng1, rng2] = 0;
+                numToDelete--;
+            }
+        }
+    }
+
+    IEnumerator deleteNumbersSlowly() {
+        // delete 56 at random
+        int numToDelete = 56;
+        // instead of 1 random between 0 and N*N, 2 random for row and col to add more randomness
+        int rng1, rng2;
+        while (numToDelete > 0)
+        {
+            rng1 = Random.Range(0, N);
+            rng2 = Random.Range(0, N);
+
+            if (intBoard[rng1, rng2] != 0) {
+                intBoard[rng1, rng2] = 0;
+                numToDelete--;
+                yield return new WaitForSeconds(delay);
+                updateBoard();
+            }
+        }
     }
 
     void updateBoard() {
@@ -82,11 +172,11 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    static bool isSafe(int row, int col, int num) {
-        return isSafeRow(row, num) && isSafeCol(col, num) && isSafeRegion(row, col, num);
+    bool isSafe(int row, int col, int num) {
+        return intBoard[row, col] == 0 && isSafeRow(row, num) && isSafeCol(col, num) && isSafeRegion(row, col, num);
     }
 
-    static bool isSafeRow(int row, int num) {
+    bool isSafeRow(int row, int num) {
         for (int i = 0; i < N; i++)
         {
             if (intBoard[row, i] == num) return false;
@@ -94,7 +184,7 @@ public class GameManager : MonoBehaviour
         return true;
     }
 
-    static bool isSafeCol(int col, int num) {
+    bool isSafeCol(int col, int num) {
         for (int i = 0; i < N; i++)
         {
             if (intBoard[i, col] == num) return false;
@@ -102,7 +192,7 @@ public class GameManager : MonoBehaviour
         return true;
     }
 
-    static bool isSafeRegion(int row, int col, int num) {
+    bool isSafeRegion(int row, int col, int num) {
         int rowStart = row - row % R;
         int colStart = col - col % R;
         for (int i = rowStart; i < rowStart + R; i++)
@@ -115,7 +205,7 @@ public class GameManager : MonoBehaviour
         return true;
     }
 
-    static bool solve(int[,] board)
+    bool solve(int[,] board, bool addToList)
     {
         // set to negative to not cause errors with static calling
         int row = -1, col = -1;
@@ -143,17 +233,80 @@ public class GameManager : MonoBehaviour
         // backtrack each row
         for (int num = 1; num <= N; num++)
         {
+            if (addToList) list.Add(new instruction(row, col, num));
             if (isSafe(row, col, num)) {
                 board[row, col] = num;
-                if (solve(board)) {
+
+                if (solve(board, addToList)) {
                     return true;
                 } 
                 else {
                     board[row, col] = 0;
+                    if (addToList) list.Add(new instruction(row, col, 0));
+                }
+            }
+            if (num == N) {
+                board[row, col] = 0;
+                if (!isSafe(row, col, N)) {
+                    board[row, col] = 0;
+                    if (addToList) list.Add(new instruction(row, col, 0));
                 }
             }
         }
         return false;
+    }
+
+    // coroutine to show step by step changes of solver algorithm
+    // coroutine instead of function to add delays freely whenever I need
+    IEnumerator showAlgorithm()
+    {
+        // list is empty, immediately leave coroutine
+        if (list.Count != 0) {
+            instruction current = list[0];
+            instruction next;
+
+            // check next instruction to see if it's on a different tile, if yes, then we will want to change the color of the tile back to default
+            bool changeColorBack = true;
+            if (list.Count > 1)
+            {
+                next = list[1];
+                if (current.row == next.row && current.col == next.col)
+                {
+                    changeColorBack = false;
+                }
+            }
+
+            int row = current.row;
+            int col = current.col;
+            int num = current.num;
+
+            int regRow = row / R;
+            int tileRow = row % R;
+
+            int regCol = col / R;
+            int tileCol = col % R;
+
+
+            GameObject region = BOARD.transform.GetChild(regRow * R + regCol).gameObject;
+            GameObject tile = region.transform.GetChild(tileRow * R + tileCol).gameObject;
+
+            tile.GetComponent<Image>().color = selectedTileColor;
+            tile.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = (num != 0) ? num.ToString() : "";
+
+            yield return new WaitForSeconds(delay);
+            if (list.Count > 1) {
+                next = list[1];
+                if (current.row * N + current.col - 1 == next.row * N + next.col) {
+                    tile.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "";
+                    yield return new WaitForSeconds(delay * 2);
+                }
+            }
+            
+            list.RemoveAt(0);
+            if (changeColorBack) tile.GetComponent<Image>().color = defaultTileColor;
+            
+            StartCoroutine("showAlgorithm");
+        }
     }
 
     // Update is called once per frame
